3a8a9940fe2ec67f88f356ded07ac4b43e91f941
Sadeed978 <sadeedahmed681@gmail.com>
2025-11-16 21:59:25 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express()
+const port = process.env.port||3000
+
+app.use (cors());
+app.use(express.json());
+const uri= `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bhxscno.mongodb.net/?appName=Cluster0`
+
+const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
+
+app.get('/', (req, res) => {
+  res.send('movie-world-server is runnging')
+})
+
+async function run() {
+    try {
+      
+      await client.connect();
+      const database = client.db("AllMovies");
+      const moviesCollection = database.collection("movies");
+      
+      app.post('/Movies',async(req,res)=>{
+         const newMovies =req.body;
+         const result = await moviesCollection.insertOne(newMovies);
+         res.send(result);
+      })
+
+      await client.db("admin").command({ ping: 1 });
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+      
+      await client.close();
+    }
+  }
+  run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log(`movie-world-server listening on port ${port}`)
+})
